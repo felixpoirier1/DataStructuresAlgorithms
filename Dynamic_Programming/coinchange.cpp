ce89fe389a694873debb8149ba83375f67d8c7ed
felixpoirier1 <felixpoirier2001@gmail.com>
2023-08-12 00:25:27 -0400

    commit

--- Dynamic_Programming/coinchange.cpp
+++ Dynamic_Programming/coinchange.cpp
@@ -2,31 +2,51 @@
  #include<iostream>
  #include<climits>
 
- 
- int coinchange(int m, int coins[], int l){
+
+ int td_coinchange(int m, int coins[], int l){
     int possibs[l] = {0};
     for (int i = 0; i < l; i++){
-        possibs[i] = INT_MAX;
+        possibs[i] = UINT16_MAX;
         if ((m - coins[i]) == 0){
             return 1;
         }
         else if ((m - coins[i]) > 0){
-           possibs[i] = coinchange(m-coins[i], coins, l) + 1;
+           possibs[i] = td_coinchange(m-coins[i], coins, l) + 1;
         }
     }
     
-    int min = INT_MAX;
+    int min = UINT16_MAX;
     for(auto i : possibs){
         min = std::min(min, i);
     }
     return min;
 }
 
+int bu_coinchange(int m, int coins[], int l){
+    int steps[m+1] = {};
+
+    for(int i=0; i <= m; i++){
+        steps[i] = UINT16_MAX;
+        for(int j=0; j < l; j++){
+            if ((i - coins[j]) > 0){
+                steps[i] = std::min(steps[i], steps[i-coins[j]] + 1);
+            }
+            else if (i == coins[j]){
+                steps[i] = 1;
+            }
+        }
+    }
+    return steps[m];
+}
+
 
 
 int main(){
-    int m = 20;
+    int m = 4312;
     int coins[4] = {15, 13, 17, 11};
 
-    std::cout << coinchange(m, coins, 4) << std::endl;
+    //std::cout << td_coinchange(m, coins, 4) << std::endl;
+    std::cout << bu_coinchange(m, coins, 4) << std::endl;
+    
+    return 0;
 }
